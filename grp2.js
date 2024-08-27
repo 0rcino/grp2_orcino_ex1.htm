bc0fa153e0fb38cc384eb43c70c6e0541bea3df7
Allen Jefferson <94884406+0rcino@users.noreply.github.com>
2024-08-27 15:41:21 +0800

    new add and changes

---
+++ grp2.js
@@ -0,0 +1,8 @@
+// code for onclick for info button
+function showInfo(containerId) {
+    document.getElementById(containerId).style.display = 'block';
+}
+
+function hideInfo(containerId) {
+    document.getElementById(containerId).style.display = 'none';
+}
